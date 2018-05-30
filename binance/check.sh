d38eeebb59fd0f68586a4c57ea6f8b1e96884e4d
Gabriel <pkguowu@sina.com>
2018-05-30 09:31:16 +0800

    cancel eos

--- binance/check.sh
+++ binance/check.sh
@@ -8,6 +8,6 @@
 source ~/.profile
 /usr/bin/python ~/coinTrade/binance/check.py -t eth -b usdt -a 3 -p 2 -r 0.08 -f 0.001
 
-/usr/bin/python ~/coinTrade/binance/check.py -t eos -b usdt -a 2 -p 3 -r 0.05 -f 0.001
+#/usr/bin/python ~/coinTrade/binance/check.py -t eos -b usdt -a 2 -p 3 -r 0.05 -f 0.001
 
 /usr/bin/python ~/coinTrade/binance/check.py -t blz -b eth -a 0 -p 8 -r 0.05 -f 0.001
